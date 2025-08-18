61675a2f1762116501d76b2f2ae790cad8a04828
pasha-il <99135065+pasha-il@users.noreply.github.com>
2025-08-18 10:24:20 +0300

    Load books and manage add-book modal in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,28 @@
 import * as React from 'react';
+import { liveQuery } from 'dexie';
+
+import { db } from './db';
+import { Book } from './types';
+import { BookList } from './components/BookList';
+import { AddBookModal } from './components/AddBookModal';
 
 export default function App() {
-  return <div>App</div>;
+  const [books, setBooks] = React.useState<Book[]>([]);
+  const [isModalOpen, setModalOpen] = React.useState(false);
+
+  React.useEffect(() => {
+    const sub = liveQuery(() => db.books.toArray()).subscribe({
+      next: setBooks,
+    });
+    return () => sub.unsubscribe();
+  }, []);
+
+  return (
+    <div>
+      <button onClick={() => setModalOpen(true)}>Add a book</button>
+      <BookList books={books} />
+      {isModalOpen && <AddBookModal onClose={() => setModalOpen(false)} />}
+    </div>
+  );
 }
+
